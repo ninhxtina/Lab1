d9dfed9505e58446ed603d48af608156f6777e1d
Christina Ninh <christinaninh07@yahoo.com>
2020-10-05 12:59:45 -0700

    added methods

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,31 @@
 public class Car {
-}
+    //instance variables
+    private String make;
+    private String model;
+    private int mph;
+
+    //getter & setter methods
+    public void setMake (String make) {
+        this.make = make;
+    }
+
+    public void setModel (String model) {
+        this.model = model;
+    }
+
+    public void setMiles (int miles) {
+        this.mph = mph;
+    }
+
+    public String getMake() {
+        return this.make;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public int getMiles() {
+        return this.mph;
+    }
+}//end car class
